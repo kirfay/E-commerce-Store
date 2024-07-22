e2406eeeada4ca4f61d0d9feb1d7cbd6d8664e96
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-07-22 11:06:39 -0700

    created the project

---
+++ ecommerce-store/src/main/java/com/capstone/ecommerce/store/EcommerceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.ecommerce.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceStoreApplication.class, args);
+	}
+
+}
