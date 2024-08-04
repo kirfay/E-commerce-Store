18ed0c60615c9467c1c2a40e007f43bc48c5f800
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-08-03 19:25:28 -0700

    created entity for product and order

--- ecommerce-store/src/main/java/com/capstone/ecommerce/store/database/entity/Product.java
+++ ecommerce-store/src/main/java/com/capstone/ecommerce/store/database/entity/Product.java
@@ -1,4 +1,31 @@
 package com.capstone.ecommerce.store.database.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "product_code")
+    private String productCode;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_description", columnDefinition="TEXT")
+    private String productDescription;
+
+    @Column(name = "buy_price", columnDefinition = "DECIMAL")
+    private Double buyPrice;
 }
